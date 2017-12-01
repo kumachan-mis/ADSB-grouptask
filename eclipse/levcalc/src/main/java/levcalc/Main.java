1e79a611f40ec0ba64ea43b65523e4ea99beb4d8
shira <shira7867@gmail.com>
2017-12-02 03:40:28 +0900

    fix インデント

--- eclipse/levcalc/src/main/java/levcalc/Main.java
+++ eclipse/levcalc/src/main/java/levcalc/Main.java
@@ -13,30 +13,30 @@ public class Main
 
 	public static void main(String[] args) throws IOException
 	{
-        File[] files =  new File(".").listFiles();
-        for(int i = 0; i < files.length; i++)
-        {
-        	System.out.println(i + ") " + files[i].getName());
-        }
+		File[] files =  new File(".").listFiles();
+		for(int i = 0; i < files.length; i++)
+		{
+			System.out.println(i + ") " + files[i].getName());
+		}
 
-        int index1, index2;
-        try(Scanner sc = new Scanner(System.in))
-        {
-            System.out.print("Select first file > ");
-            index1 = sc.nextInt();
-            System.out.print("Select second file > ");
-            index2 = sc.nextInt();
-        }
+		int index1, index2;
+		try(Scanner sc = new Scanner(System.in))
+		{
+			System.out.print("Select first file > ");
+			index1 = sc.nextInt();
+			System.out.print("Select second file > ");
+			index2 = sc.nextInt();
+		}
 
-        String str1, str2;
-        try( BufferedReader br = new BufferedReader(new FileReader(files[index1])))
-        {
-        	str1 = br.readLine();
-        }
-        try( BufferedReader br = new BufferedReader(new FileReader(files[index2])))
-        {
-        	str2 = br.readLine();
-        }
+		String str1, str2;
+		try( BufferedReader br = new BufferedReader(new FileReader(files[index1])))
+		{
+			str1 = br.readLine();
+		}
+		try( BufferedReader br = new BufferedReader(new FileReader(files[index2])))
+		{
+			str2 = br.readLine();
+		}
 
 
 		System.out.println(new LevensteinDistance().getDistance(str1, str2));
