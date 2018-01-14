050c89c88a6de40ec58219d53b318f35e5e83ab9
kumachan-mis <yuyachorus-97.11nov@ezweb.ne.jp>
2018-01-15 01:57:47 +0900

    re-format

--- src/Reserve_code/reserve_main.c
+++ src/Reserve_code/reserve_main.c
@@ -4,54 +4,60 @@
 #include "linkedlist.h"
 
 #define maxTlen 105000
-#define Snum    20000
-#define Slen    25
+#define Snum 20000
+#define Slen 25
 
 int main()
 {
-    list * T = make_new_list();
+    list *T = make_new_list();
     char Tstring[maxTlen + 1];
-    list * S = make_new_list();
+    list *S = make_new_list();
     char Sstring[Slen + 1];
 
-    node * p;
-    list * part = make_new_list();
-    part -> length = 20;
-
+    node *p;
+    list *part = make_new_list();
+    part->length = 20;
 
     scanf("%s", Tstring);
 
     const int Tlen = strlen(Tstring);
     T = insert_data(T, Tstring, -1);
-    p = T -> head;
+    p = T->head;
 
     int i;
     int index = 0;
-   
-    for(i = 0; i < Snum/2; i++){
+
+    for (i = 0; i < Snum / 2; i++)
+    {
         scanf("%s", Sstring);
         Sstring[20] = '\0';
 
-        if(i == 0){
+        if (i == 0)
+        {
             S = insert_data(S, Sstring, -1);
-            S -> length = 20;
-        }else{
+            S->length = 20;
+        }
+        else
+        {
             S = replace_data(S, Sstring, 0);
         }
 
-        for(index = 0; index < Tlen - Slen + 1; index++){
-            if(p == NULL) break;
-            part -> head = p;
+        for (index = 0; index < Tlen - Slen + 1; index++)
+        {
+            if (p == NULL)
+                break;
+            part->head = p;
 
-            if(edit_distance(part, S, 2)){
+            if (edit_distance(part, S, 2))
+            {
                 replace_data(T, Sstring, index);
                 break;
             }
-            p = p -> next;
+            p = p->next;
         }
-         p = T -> head;
+        p = T->head;
     }
-    
+
     show(T);
     return 0;
 }
